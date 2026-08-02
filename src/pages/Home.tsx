8732e5e61b23360271bbd499b0315ce1478fc159
wissalsoma7-cyber <wissalsoma7@gmail.com>
2026-08-02 02:17:40 +0100

    Create Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,28 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        padding: 20,
+        background: "#F4F9FF",
+        minHeight: "100vh"
+      }}
+    >
+      <h1
+        style={{
+          color: "#0A84FF",
+          textAlign: "center"
+        }}
+      >
+        NÉO LAVAGE
+      </h1>
+
+      <button>Continuer avec Google</button>
+
+      <button>Continuer avec Apple</button>
+
+      <button>Essayer sans e-mail</button>
+
+      <button>Partager Néo Lavage</button>
+    </div>
+  );
+}
