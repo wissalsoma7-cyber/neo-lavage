33475008ad0f8889895e4694346c04cb7bed6c35
wissalsoma7-cyber <wissalsoma7@gmail.com>
2026-08-02 08:37:26 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,5 @@
 import Splash from "./pages/Splash";
-import Home from "./pages/Home";
-import GoogleLogin from "./pages/GoogleLogin";
-import Notifications from "./pages/Notifications";
-import Location from "./pages/Location";
-import GoogleMaps from "./pages/GoogleMaps";
-import VehicleSelection from "./pages/VehicleSelection";
-import Booking from "./pages/Booking";
-import WasherProfile from "./pages/WasherProfile";
-import Confirmation from "./pages/Confirmation";
-import History from "./pages/History";
-import Settings from "./pages/Settings";
 
 export default function App() {
-  return (
-    <Splash />
-  );
+  return <Splash />;
 }
