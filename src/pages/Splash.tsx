e4e4cd4cbef117744ecbe0dad935076b91b3bc2c
wissalsoma7-cyber <wissalsoma7@gmail.com>
2026-08-02 02:14:35 +0100

    Create Splash.tsx

---
+++ src/pages/Splash.tsx
@@ -0,0 +1,20 @@
+export default function Splash() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        background: "#F4F9FF"
+      }}
+    >
+      <h1 style={{ color: "#0A84FF" }}>NÉO LAVAGE</h1>
+
+      <p>Lavage professionnel à domicile</p>
+
+      <p>Chargement...</p>
+    </div>
+  );
+}
