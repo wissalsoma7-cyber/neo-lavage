fdeaf8b163940850bed138a7d4b5f540c1b76f6f
wissalsoma7-cyber <wissalsoma7@gmail.com>
2026-08-02 06:06:55 +0100

    Create GoogleLogin.tsx

---
+++ src/pages/GoogleLogin.tsx
@@ -0,0 +1,25 @@
+export default function GoogleLogin() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        background: "#F4F9FF",
+        padding: 20
+      }}
+    >
+      <h2>Connexion Google</h2>
+
+      <button>Choisir un compte Google</button>
+
+      <button>Utiliser un autre compte</button>
+
+      <button>Annuler</button>
+
+      <button>Continuer</button>
+    </div>
+  );
+}
