59f4af0cf281df2176d9ea58a996abc926a3bfaa
wissalsoma7-cyber <wissalsoma7@gmail.com>
2026-08-02 05:39:28 +0100

    Update Notifications.tsx

--- src/pages/Notifications.tsx
+++ src/pages/Notifications.tsx
@@ -1,1 +1,25 @@
+export default function Notifications() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        background: "#F4F9FF",
+        padding: 20
+      }}
+    >
+      <h2>Notifications</h2>
 
+      <p>
+        Néo Lavage souhaite vous envoyer des notifications concernant vos réservations et l'arrivée du laveur.
+      </p>
+
+      <button>Autoriser</button>
+
+      <button>Plus tard</button>
+    </div>
+  );
+}
