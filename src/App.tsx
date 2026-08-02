0c4b0e662218e8763b31cb9888853517d73c08ce
wissalsoma7-cyber <wissalsoma7@gmail.com>
2026-08-02 10:12:10 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,24 @@ import { useState } from "react";
 
 import Home from "./pages/Home";
 import GoogleLogin from "./pages/GoogleLogin";
+import Notifications from "./pages/Notifications";
 
 export default function App() {
-  const [page, setPage] = useState<"home" | "google">("home");
+  const [page, setPage] =
+    useState<"home" | "google" | "notifications">("home");
 
   if (page === "google") {
-    return <GoogleLogin />;
+    return (
+      <GoogleLogin
+        onContinue={() => {
+          setPage("notifications");
+        }}
+      />
+    );
+  }
+
+  if (page === "notifications") {
+    return <Notifications />;
   }
 
   return (
