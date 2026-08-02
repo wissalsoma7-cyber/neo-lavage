d2d71614537f2c32ed10b38eebd9df2dcb9ec1f8
wissalsoma7-cyber <wissalsoma7@gmail.com>
2026-08-02 09:47:40 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import GoogleLogin from "./pages/GoogleLogin";
 
 export default function App() {
-  const [page, setPage] = useState("home");
+  const [page, setPage] = useState<"home" | "google">("home");
 
   if (page === "google") {
     return <GoogleLogin />;
