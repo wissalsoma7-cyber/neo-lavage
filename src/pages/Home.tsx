655418c852d1d42f334665c559a4c1c9c944d7ca
wissalsoma7-cyber <wissalsoma7@gmail.com>
2026-08-02 13:30:28 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,62 @@
-export default function 
-  Home({ onGoogle }: { onGoogle: () => 
-                      void}) {
+export default function Home({
+  onGoogle,
+}: {
+  onGoogle: () => void;
+}) {
   return (
     <div
       style={{
         minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
         background: "#F4F9FF",
-        padding: 20
+        padding: 24,
       }}
     >
-      <h1
-        style={{
-          textAlign: "center",
-          color: "#0A84FF"
-        }}
-      >
+      <h1 style={{ color: "#0A84FF" }}>
         NÉO LAVAGE
       </h1>
 
+      <p>
+        Le lavage de véhicules à domicile
+      </p>
+
+      <br />
+
       <button onClick={onGoogle}>
-  Continuer avec Google
-</button>
+        Continuer avec Google
+      </button>
 
-      <button>Continuer avec Apple</button>
+      <br />
 
-      <button>Essayer sans e-mail</button>
+      <button>
+        Continuer avec Apple
+      </button>
 
-      <button>Partager Néo Lavage</button>
+      <br />
+
+      <button>
+        Continuer avec votre adresse e-mail
+      </button>
+
+      <br />
+
+      <p>
+        Vous avez déjà un compte ?
+      </p>
+
+      <button>
+        Se connecter
+      </button>
+
+      <br />
+
+      <small style={{ textAlign: "center" }}>
+        En continuant, vous acceptez les Conditions d'utilisation
+        et la Politique de confidentialité.
+      </small>
     </div>
   );
 }
