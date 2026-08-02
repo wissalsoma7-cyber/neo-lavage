89da019fdf8a8f9754fdbf51c19d14ad26972f27
wissalsoma7-cyber <wissalsoma7@gmail.com>
2026-08-02 01:45:55 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#ffffff",
+        fontFamily: "Arial"
+      }}
+    >
+      <div style={{ textAlign: "center" }}>
+        <h1 style={{ color: "#0A84FF" }}>NÉO LAVAGE</h1>
+        <p>Lavage professionnel à domicile</p>
+
+        <button>Continuer avec Google</button>
+
+        <br /><br />
+
+        <button>Continuer avec Apple</button>
+
+        <br /><br />
+
+        <button>Essayer sans e-mail</button>
+
+        <br /><br />
+
+        <button>Partager Néo Lavage</button>
+      </div>
+    </div>
+  );
+}
