0b8e211829e705b24fde1ec25fda78e92cf98a4f
wissalsoma7-cyber <wissalsoma7@gmail.com>
2026-08-05 00:25:31 +0100

    Update History.tsx

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -2,26 +2,30 @@ export default function History({
   onContinue,
 }: {
   onContinue: () => void;
-})
-  <button onClick={onContinue}>
-  Évaluer ce lavage
-</button>
-    {<div
+}) {
+  return (
+    <div
       style={{
         height: "100vh",
         display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
         flexDirection: "column",
+        justifyContent: "center",
+        padding: 20,
         background: "#F4F9FF",
-        padding: 20
+        gap: 20,
       }}
     >
       <h2>Historique</h2>
 
-      <p>Vos réservations apparaîtront ici.</p>
+      <p>15/08/2026 - Lavage Voiture - 120 DH</p>
 
-      <button>Retour</button>
+      <p>10/08/2026 - Lavage SUV - 150 DH</p>
+
+      <p>02/08/2026 - Lavage Moto - 60 DH</p>
+
+      <button onClick={onContinue}>
+        Donner une évaluation
+      </button>
     </div>
   );
 }
